801975f3957469b05dd8b8b6dae48697829f55bc
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-05-11 18:55:10 +0300

    Create Basics 15.js

---
+++ Basics/Basics 15.js
@@ -0,0 +1,7 @@
+//If a is smaller than b, divide a by b
+// Otherwise, multiply both numbers
+// Return the resulting value
+function myFunction(a, b) {
+
+              return a < b ? a / b : a * b
+}
