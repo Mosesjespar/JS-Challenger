1cb8f2b992b596b072de86c672f7972a979a2ac4
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-05-11 21:31:09 +0300

    Create Arrays 03.js

---
+++ Arrays/Arrays 03.js
@@ -0,0 +1,11 @@
+// Write a function that takes an array (a) as argument
+// Extract the last 3 elements of a
+// Return the resulting array
+function myFunction(a) {
+
+              return a.slice(-3);
+
+
+              //the slice() method removes elements of an array
+              //the parameter -3 removes the last 3 elements.....if it was 3,it would remove the first 3
+}
